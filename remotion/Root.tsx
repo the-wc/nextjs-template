2d2a2e6e78dae849694a74b43fb9bd34e8a79e6f
Will <william.kenyonc@gmail.com>
2025-10-02 13:59:43 -0700

    adds remotion config

---
+++ remotion/Root.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Composition } from "remotion";
+import { COMP_NAME, Main } from "./composition/Main";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id={COMP_NAME}
+        component={Main}
+        durationInFrames={60}
+        fps={30}
+        width={1280}
+        height={720}
+        defaultProps={{ title: "stranger" }}
+      />
+    </>
+  );
+};
---
+++ remotion/composition/Main.tsx
@@ -0,0 +1,74 @@
+import { z } from "zod";
+import {
+  AbsoluteFill,
+  Sequence,
+  spring,
+  useCurrentFrame,
+  useVideoConfig,
+} from "remotion";
+
+import { NextLogo } from "./NextLogo";
+import { loadFont, fontFamily } from "@remotion/google-fonts/Inter";
+import React from "react";
+import { Rings } from "./Rings";
+import { TextFade } from "./TextFade";
+
+export const COMP_NAME = "MyComp";
+
+export const CompositionProps = z.object({
+  title: z.string(),
+});
+
+export const defaultMyCompProps: z.infer<typeof CompositionProps> = {
+  title: "Next.js and Remotion",
+};
+
+export const DURATION_IN_FRAMES = 200;
+export const VIDEO_WIDTH = 1280;
+export const VIDEO_HEIGHT = 720;
+export const VIDEO_FPS = 30;
+
+loadFont("normal", {
+  subsets: ["latin"],
+  weights: ["400", "700"],
+});
+export const Main = ({ title }: z.infer<typeof CompositionProps>) => {
+  const frame = useCurrentFrame();
+  const { fps } = useVideoConfig();
+
+  const transitionStart = 2 * fps;
+  const transitionDuration = 1 * fps;
+
+  const logoOut = spring({
+    fps,
+    frame,
+    config: {
+      damping: 200,
+    },
+    durationInFrames: transitionDuration,
+    delay: transitionStart,
+  });
+
+  return (
+    <AbsoluteFill className="bg-white">
+      <Sequence durationInFrames={transitionStart + transitionDuration}>
+        <Rings outProgress={logoOut}></Rings>
+        <AbsoluteFill className="justify-center items-center">
+          <NextLogo outProgress={logoOut}></NextLogo>
+        </AbsoluteFill>
+      </Sequence>
+      <Sequence from={transitionStart + transitionDuration / 2}>
+        <TextFade>
+          <h1
+            className="text-[70px] font-bold"
+            style={{
+              fontFamily,
+            }}
+          >
+            {title}
+          </h1>
+        </TextFade>
+      </Sequence>
+    </AbsoluteFill>
+  );
+};
