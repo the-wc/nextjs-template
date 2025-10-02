a1a02ea2b9a0662cfe9bc9397cfda619478de12e
Will <william.kenyonc@gmail.com>
2025-10-02 14:01:11 -0700

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import remotion from "@remotion/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +10,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+const [nextPlugin] = compat.extends("plugin:@next/next/recommended");
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/typescript"),
+  nextPlugin,
+  {
+    ...remotion.flatPlugin,
+    rules: {
+      ...remotion.flatPlugin.rules,
+      ...Object.entries(nextPlugin.rules).reduce((acc, [key]) => {
+        return { ...acc, [key]: "off" };
+      }, {}),
+    },
+    files: ["src/remotion/**"],
+  },
 ];
 
 export default eslintConfig;
