239134bbd51591d4adfef6449425bb85d6b54426
Will <william.kenyonc@gmail.com>
2025-10-03 10:35:23 -0700

    adds linting

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,9 @@
 
 import { Player } from "@remotion/player";
 import type { NextPage } from "next";
-import React, { useMemo, useState } from "react";
-import { z } from "zod";
+import React from "react";
 import {
   defaultMyCompProps,
-  CompositionProps,
   DURATION_IN_FRAMES,
   VIDEO_FPS,
   VIDEO_HEIGHT,
@@ -16,36 +14,24 @@ import {
 import { Main } from "../remotion/composition/Main";
 
 const Home: NextPage = () => {
-  const [text, setText] = useState<string>(defaultMyCompProps.title);
-
-  const inputProps: z.infer<typeof CompositionProps> = useMemo(() => {
-    return {
-      title: text,
-    };
-  }, [text]);
-
   return (
     <div>
-      <div className="max-w-screen-md m-auto mb-5">
-        <div className="overflow-hidden rounded-geist shadow-[0_0_200px_rgba(0,0,0,0.15)] mb-10 mt-16">
-          <Player
-            component={Main}
-            inputProps={inputProps}
-            durationInFrames={DURATION_IN_FRAMES}
-            fps={VIDEO_FPS}
-            compositionHeight={VIDEO_HEIGHT}
-            compositionWidth={VIDEO_WIDTH}
-            style={{
-              // Can't use tailwind class for width since player's default styles take presedence over tailwind's,
-              // but not over inline styles
-              width: "100%",
-            }}
-            controls
-            autoPlay
-            loop
-          />
-        </div>
-      </div>
+      <Player
+        component={Main}
+        inputProps={defaultMyCompProps}
+        durationInFrames={DURATION_IN_FRAMES}
+        fps={VIDEO_FPS}
+        compositionHeight={VIDEO_HEIGHT}
+        compositionWidth={VIDEO_WIDTH}
+        style={{
+          // Can't use tailwind class for width since player's default styles take presedence over tailwind's,
+          // but not over inline styles
+          width: "100%",
+        }}
+        controls
+        autoPlay
+        loop
+      />
     </div>
   );
 };
