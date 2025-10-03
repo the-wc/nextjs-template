8c0a999e3519923cf4265c575af451ce820a6d4a
Will <william.kenyonc@gmail.com>
2025-10-03 16:55:37 -0700

    remove dev overlay from next

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  devIndicators: {
+    buildActivity: false,
+  },
 };
 
 export default nextConfig;
